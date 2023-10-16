c19fcc6888424f5a73255500cf0a5e344d2ed192
Kael <khang.tran@mozox.com>
2023-10-16 13:22:57 +0700

    change url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://ruby-chat-git-main-piskael03.vercel.app/"
+          href="https://ruby-chat.vercel.app/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
